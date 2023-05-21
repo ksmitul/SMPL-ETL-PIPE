d53cca8fd7da7e16594102b10cfe58142c5a991b
Mitul <39934813+ksmitul@users.noreply.github.com>
2023-05-21 19:59:51 -0400

    delete

--- load-dt.py
+++ load-dt.py
@@ -1,38 +1,40 @@
 import boto3
 import xml.etree.ElementTree as et
 import pandas as pd
-import numpy as np
-import mysql.connector
 from sqlalchemy import create_engine
+import configparser
+
+config=configparser.ConfigParser()
+
+config.read('config.ini')
+
+host = config.get('host', 'host')
+user = config.get('user', 'user')
+password = config.get('password', 'password')
+database = config.get('database', 'database')
 
 
-# Establish a connection
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{database}')
 
-host='personal-demo-db.cnw1czgvgwru.us-east-1.rds.amazonaws.com',
-user='admin',
-password='6416711506',
-database='reed'
+# Define the S3 bucket and key
+bucket_name = 'reading-loading-xml'
+key_name = 'reed.xml'
 
-engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
+# Retrieve the XML data from S3
+s3_client = boto3.client('s3')
+response = s3_client.get_object(Bucket=bucket_name, Key=key_name)
+data = response['Body'].read().decode('utf-8')
 
-
-s3_client= boto3.client('s3')
-
-bucket_name='reading-loading-xml'
-key_name='reed.xml'
-
-reponse=s3_client.get_object(Bucket=bucket_name, Key=key_name)
-
-data= reponse['Body'].read().decode('utf-8')
-
-
+# Parse the XML data
 tree = et.ElementTree(et.fromstring(data))
 root = tree.getroot()
 
+# Prepare lists to store data
 course_data = []
 time_data = []
 place_data = []
 
+# Extract course, time, and place data from XML
 for element in root:
     reg_num = element.find('reg_num').text
     subj = element.find('subj').text
@@ -42,7 +44,7 @@ for element in root:
     units = element.find('units').text
     instructor = element.find('instructor').text
     days = element.find('days').text
-    
+
     course_data.append((reg_num, subj, crse, sect, title, units, instructor, days))
 
     for time in element.iter('time'):
@@ -60,11 +62,18 @@ course_df = pd.DataFrame(course_data, columns=['reg_num', 'subj', 'crse', 'sect'
 time_df = pd.DataFrame(time_data, columns=['start_time', 'end_time'])
 place_df = pd.DataFrame(place_data, columns=['building', 'room'])
 
-course_df.to_sql(name='courses', con=engine, if_exists='append', index=False)
+# Define table names
+course_table_name = 'courses'
+time_table_name = 'times'
+place_table_name = 'place'
 
-time_df.to_sql(name='times', con=engine, if_exists='append', index=False)
+# Insert DataFrames into MySQL tables
+course_df.to_sql(name=course_table_name, con=engine, if_exists='replace', index=False)
+time_df.to_sql(name=time_table_name, con=engine, if_exists='append', index=False)
+place_df.to_sql(name=place_table_name, con=engine, if_exists='append', index=False)
 
-place_df.to_sql(name='place', con=engine, if_exists='append', index=False)
-
+# Dispose the engine
 engine.dispose()
+
+
 
